ca08c16904e6ef2f2e199b11443beb0174ebc831
hilla-sb <hillsb2302@gmail.com>
2022-08-31 20:25:41 +0200

    style-navbar

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import background from '../imagenes/background.png';
 import {Grid} from "@mui/material";
-
+// import '../App.css';
 export default function Header () {
 
     return (
 <Grid >
-                <img src={background} alt="" style={{width: "100%", }}/>
+                <img src={background} alt="" style={{width: "100%",opacity:"0.8" }}/>
             </Grid>
         
         )
--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Stack, Button, Box} from "@mui/material";
+import { AppBar, Toolbar, Button, Box} from "@mui/material";
 import logo from '../imagenes/logo.png';
+import '../App.css';
 
 export default function Navbar () {
     return (
-        <AppBar position='fixed' style={{backgroundColor: "transparent", background: "linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(47,48,58,0.742734593837535) 37%, rgba(47,48,58,1) 86%)", boxShadow: "0px 0px 0px 0px", paddingTop: "2%"}}>
+        <AppBar  className ="barra"position='fixed'>
             <Toolbar>
-                <img src={logo} alt="Logo" style={{ width:"10rem" , marginLeft: "2rem"}} />
+                <img className = "logo2" src={logo} alt="Logo2" />
                 
-                <Box style={{marginRight: "0.5rem"}}>
-                <Button href="#Main2" style={{color: "white", fontFamily: "Poppins", fontWeight: "300", fontSize: "2rem", textTransform: "capitalize", marginRight: "5rem"}}>Inicio</Button>
-                <Button href="#Main2" style={{color: "white", fontFamily: "Poppins", fontWeight: "300", fontSize: "2rem", textTransform: "capitalize", marginRight: "5rem"}}>Acerca de mi</Button>
-                <Button href="#Main2" style={{color: "white", fontFamily: "Poppins", fontWeight: "300", fontSize: "2rem", textTransform: "capitalize", marginRight: "5rem"}}>Lenguajes</Button>
-                <Button href="#Main2" style={{color: "white", fontFamily: "Poppins", fontWeight: "300", fontSize: "2rem", textTransform: "capitalize", marginRight: "5rem"}}>Portafolio</Button>
-                <Button href="#Main2" style={{color: "white", fontFamily: "Poppins", fontWeight: "300", fontSize: "2rem", textTransform: "capitalize", marginRight: "5rem"}}>Contactame</Button>
+                <Box className="navbar-title">
+                  
+                <Button href="#Main2" >Inicio</Button>
+                <Button href="#Main2" >Acerca de mi</Button>
+                <Button href="#Main2" >Lenguajes</Button>
+                <Button href="#Main2">Portafolio</Button>
+                <Button href="#Main2" >Contactame</Button>
                     
                     
                 </Box>
